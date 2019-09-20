d71d7992060495025c96f30ab784682d0a7d956e
Fredrik Meltveit <emkf.meltveitf@gmail.com>
2019-09-20 04:43:51 +0200

    Initialize repository

---
+++ src/main/java/com/fredrik/roombooking/RoombookingApplication.java
@@ -0,0 +1,13 @@
+package com.fredrik.roombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoombookingApplication.class, args);
+    }
+
+}

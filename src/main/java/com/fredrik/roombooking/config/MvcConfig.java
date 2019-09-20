8e1e5944b37c9c3e0e038f9d6fcbc74dd87782b6
Fredrik Meltveit <emkf.meltveitf@gmail.com>
2019-09-20 04:48:43 +0200

    Add index and mvc config

---
+++ src/main/java/com/fredrik/roombooking/config/MvcConfig.java
@@ -0,0 +1,13 @@
+package com.fredrik.roombooking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class MvcConfig {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+}

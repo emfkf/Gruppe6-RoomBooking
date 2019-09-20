1eebdea4e65b867101a2e053232687a4d102df26
Fredrik Meltveit <emkf.meltveitf@gmail.com>
2019-09-20 05:09:39 +0200

    Add web security config and login form

--- src/main/java/com/fredrik/roombooking/config/MvcConfig.java
+++ src/main/java/com/fredrik/roombooking/config/MvcConfig.java
@@ -2,12 +2,15 @@ package com.fredrik.roombooking.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/room/all").setViewName("room_all");
     }
 
 }
